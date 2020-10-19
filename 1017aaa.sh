13a0a2f0257bff377908b57ae9d59555d8182779
zhusenior <wsy5138@163.com>
2020-10-19 21:39:30 +0800

    hello.html

---
+++ 1017aaa.sh
@@ -0,0 +1,7 @@
+mkdir aaa
+touch aaa/ccc.pdf
+cd aaa
+touch c.pdf
+mkdir bbb
+cd  bbb
+echo hello > d.pdf
---
+++ 1017test.sh
@@ -0,0 +1,6 @@
+echo 正在执行任务...
+sleep 5
+mkdir 课程
+cd 课程
+echo hello > hello.txt
+echo world > world.txt
